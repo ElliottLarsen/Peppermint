589ecbc14f06c97d3c40161e775f32a91c28bb81
Josquin Larsen <jsqnlrsn@gmail.com>
2024-12-21 19:34:10 -0500

    started on accounts

---
+++ app/src/components/ViewAccounts.js
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'
+
+const ViewAccounts = () => {
+    const [accounts, setAccounts] = useState(null);
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchAccounts = async() => {
+            try {
+                const token = localStorage.getItem('token');
+                const response = await axios.get('http://127.0.0.1:8000/peppermint/account/my_accounts', {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+                setAccounts(response.data);
+            } catch (error) {
+                console.error('Error retrieving accounts.', error);
+                if (error.response.status === 401) {
+                    navigate('/login');
+                }
+            }
+        };
+        fetchAccounts();
+    }, []);
+
+    if (!accounts) {
+        return <div><p>No account info available.</p></div>;
+    }
+
+    return (
+        <>
+        <div class="page-title">
+            <h2>Accounts</h2>
+        </div>
+        <div>
+            <ul>
+                {accounts.map(account => (
+                    <li key={account.id}>
+                        <p>{account.institution}</p>
+                        <p>{account.account_type}</p>
+                        <p>{account.current_balance}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+        </>
+    )
+};
+
+export default ViewAccounts;
--- app/src/pages/User.js
+++ app/src/pages/User.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'
 import { MdOutlineEdit } from "react-icons/md";
+import ViewAccounts from '../components/ViewAccounts';
 
 const Profile = () => {
     const [userData, setUserData] = useState(null);
@@ -43,9 +44,11 @@ const Profile = () => {
 
         return (
             <>
+            
             <div class="page-title">
                 <h2>User Profile</h2>
             </div>
+            <div class="user-landing">
             <div class="profile">
                 {/* create table? */}
                 <p>Username: {userData.username}</p>
@@ -53,7 +56,10 @@ const Profile = () => {
                 <p>Email: {userData.email}</p>
                 <i class="edit-button" alt="Edit User"><MdOutlineEdit onClick={() => navigate('/user/edit')} /></i>
             </div>
-
+            <div class='account-card'>
+                <ViewAccounts />
+            </div>
+            </div>
             </>
         )
 };
